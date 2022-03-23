0b27980989d01ccb94f7556b255d48f9f742c0ac
JaiPi <29052214+JaiPi@users.noreply.github.com>
2022-03-23 00:14:57 +0000

    Add bookstore

---
+++ DataDispatcher/src/test/java/pt/iscteiul/datadispatcher/DataDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iscteiul.datadispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDispatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
