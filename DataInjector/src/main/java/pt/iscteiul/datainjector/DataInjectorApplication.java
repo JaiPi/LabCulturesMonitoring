ff4534caa89d9dbc4c04271b255044a691541aab
JaiPi <29052214+JaiPi@users.noreply.github.com>
2022-03-25 01:09:31 +0000

    Add DataInjector

---
+++ DataInjector/src/main/java/pt/iscteiul/datainjector/DataInjectorApplication.java
@@ -0,0 +1,13 @@
+package pt.iscteiul.datainjector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataInjectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataInjectorApplication.class, args);
+    }
+
+}
