b1a7fd3bc5f97b2f5dae982a304d70b38c643ed9
JaiPi <29052214+JaiPi@users.noreply.github.com>
2022-03-25 18:24:37 +0000

    Add DataInjector

---
+++ DataInjector/src/test/java/pt/iscteiul/datainjector/DataInjectorApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iscteiul.datainjector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataInjectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
