e75fa0d9f1b1eafe0e8d13db1853ce2baba93e75
JaiPi <29052214+JaiPi@users.noreply.github.com>
2022-05-03 23:02:29 +0100

    Pause Hive MQTT feature

--- DataDispatcher/src/main/java/pt/iscteiul/datadispatcher/mqtt/hivemq/HiveMQ.java
+++ DataDispatcher/src/main/java/pt/iscteiul/datadispatcher/mqtt/hivemq/HiveMQ.java
@@ -1,47 +1,47 @@
-package pt.iscteiul.datadispatcher.mqtt.hivemq;
-
-import com.google.gson.Gson;
-import com.hivemq.client.mqtt.MqttClient;
-import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
-import org.springframework.context.annotation.Configuration;
-import pt.iscteiul.datadispatcher.model.SensorData;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-
-@Configuration
-public class HiveMQ {
-
-    public Mqtt5BlockingClient mqtt5BlockingClient() {
-        final String host = "1b741dfb7dbc441fb203b6490be6b541.s1.eu.hivemq.cloud";
-        final String username = "group10";
-        final String password = "piSID2022";
-
-        //create an MQTT client
-        final Mqtt5BlockingClient client = MqttClient.builder()
-                .useMqttVersion5()
-                .serverHost(host)
-                .serverPort(8883)
-                .sslWithDefaultConfig()
-                .buildBlocking();
-
-        //connect to HiveMQ Cloud with TLS and username/pw
-        client.connectWith()
-                .simpleAuth()
-                .username(username)
-                .password(UTF_8.encode(password))
-                .applySimpleAuth()
-                .send();
-
-        return client;
-    }
-
-    public void messageSender(SensorData medicao) {
-        String message = new Gson().toJson(medicao);
-        System.out.println("Message sent: " + message);
-        mqtt5BlockingClient().publishWith()
-                .topic("sid2022/group10")
-                .payload(UTF_8.encode(message))
-                .send();
-    }
-
-}
+//package pt.iscteiul.datadispatcher.mqtt.hivemq;
+//
+//import com.google.gson.Gson;
+//import com.hivemq.client.mqtt.MqttClient;
+//import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
+//import org.springframework.context.annotation.Configuration;
+//import pt.iscteiul.datadispatcher.model.SensorData;
+//
+//import static java.nio.charset.StandardCharsets.UTF_8;
+//
+//@Configuration
+//public class HiveMQ {
+//
+//    public Mqtt5BlockingClient mqtt5BlockingClient() {
+//        final String host = "1b741dfb7dbc441fb203b6490be6b541.s1.eu.hivemq.cloud";
+//        final String username = "group10";
+//        final String password = "piSID2022";
+//
+//        //create an MQTT client
+//        final Mqtt5BlockingClient client = MqttClient.builder()
+//                .useMqttVersion5()
+//                .serverHost(host)
+//                .serverPort(8883)
+//                .sslWithDefaultConfig()
+//                .buildBlocking();
+//
+//        //connect to HiveMQ Cloud with TLS and username/pw
+//        client.connectWith()
+//                .simpleAuth()
+//                .username(username)
+//                .password(UTF_8.encode(password))
+//                .applySimpleAuth()
+//                .send();
+//
+//        return client;
+//    }
+//
+//    public void messageSender(SensorData medicao) {
+//        String message = new Gson().toJson(medicao);
+//        System.out.println("Message sent: " + message);
+//        mqtt5BlockingClient().publishWith()
+//                .topic("sid2022/group10")
+//                .payload(UTF_8.encode(message))
+//                .send();
+//    }
+//
+//}
